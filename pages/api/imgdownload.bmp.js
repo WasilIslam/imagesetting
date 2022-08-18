33328f970885f1d11ec91d27e57725aea58a0f40
wasilislam <wasilislam456@gmail.com>
2022-08-18 12:25:32 +0500

    imgdownload

---
+++ pages/api/imgdownload.bmp.js
@@ -0,0 +1,42 @@
+import connectMongo from "../../utils/connectMongo";
+import Link from "../../models/linkModel";
+import NextCors from "nextjs-cors";
+import axios from "axios";
+
+/**
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+  await connectMongo();
+  if (req.method == "GET") {
+    const { path } = req.query;
+    console.log(path);
+    const data = await Link.findOne({ path });
+    let URL = data.link;
+    const response = await axios.get(URL, { responseType: "arraybuffer" });
+    const buffer = Buffer.from(response.data, "utf-8");
+    res.status(200).send(buffer);
+  } else if (req.method == "POST") {
+    try {
+      if (!req.body) {
+        throw Error("Body is NULL");
+      }
+      const { path, link } = JSON.parse(req.body);
+      console.log(path);
+      const x = await Link.findOne({ path });
+      x.link = link;
+      await x.save();
+      res.json("OK");
+    } catch (error) {
+      console.log(error);
+      res.json({ error });
+    }
+  }
+}
